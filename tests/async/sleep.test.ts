613c50a73fe66a39971e109c7993f4f5620111fd
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2025-06-09 17:42:52 -0700

    added spyOn to see if settimeout was called or not

--- tests/async/sleep.test.ts
+++ tests/async/sleep.test.ts
@@ -4,15 +4,18 @@ describe('sleep', () => {
 
     beforeEach(() => {
         jest.useFakeTimers()
+        jest.spyOn(global, 'setTimeout')
     })
 
     afterEach(() => {
         jest.useRealTimers()
+        jest.clearAllMocks()
     })
 
     it('should resolve value after 1 second', async () => {
         const promise = sleep(1000)
 
+        expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 1000)
         jest.advanceTimersByTime(1000)
         await expect(promise).resolves.toBe('🍀 Success')
     })
